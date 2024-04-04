9db431891a6b3909e88c205a74a56193919f7692
prof_riemke <riemke-gurzki@hdm-stuttgart.de>
2024-04-04 11:18:48 +0200

    04.04.-2

--- 07_blog/blog/app/controllers/posts_controller.ts
+++ 07_blog/blog/app/controllers/posts_controller.ts
@@ -6,4 +6,21 @@ export default class PostsController {
         const posts = await db.from('posts').select('*')
         return view.render('pages/home', { posts })
     }
+
+    public async createForm({ view }: HttpContext) {
+        return view.render('pages/posts/create_form')
+    }
+
+    public async createProcess({ request, response }: HttpContext) {
+        const title = request.input('title')
+        const text = request.input('text')
+        const result = await db.table('posts').insert({ title, text, user_id: 1 })
+        console.log(result)
+        return response.redirect('/')
+    }
+
+    public async show({ view, params }: HttpContext) {
+        const post = await db.from('posts').select('*').where('id', params.id).first()
+        return view.render('pages/posts/show',  { post } )
+    }
 }
--- 07_blog/blog/start/routes.ts
+++ 07_blog/blog/start/routes.ts
@@ -11,3 +11,8 @@ import router from '@adonisjs/core/services/router'
 import PostsController from '../app/controllers/posts_controller.js'
 
 router.get('/', [PostsController, 'index'])
+
+router.get('/posts', [PostsController, 'createForm'])
+router.post('/posts', [PostsController, 'createProcess'])
+
+router.get('/post/:id', [PostsController, 'show'])
